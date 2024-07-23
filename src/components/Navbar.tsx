362303e6fd1dfc23a4d3bf8b2750883a3099c535
wajdi19 <wajdibejaoui50@gmail.com>
2024-07-23 14:50:56 +0100

    Implement dark mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
-import logo from "../assets/game-controller.png";
+import logo from "../assets/game-controller-orange-no-bg.png";
+import ColorModeSwitch from "./ColorModeSwitch";
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px"></Image>
-      <Text>Navbar</Text>
+      <Text>
+        <ColorModeSwitch></ColorModeSwitch>
+      </Text>
     </HStack>
   );
 };
