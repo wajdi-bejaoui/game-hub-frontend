9116bdb5d77c3183017a5a47c4946332713c4aa8
wajdi19 <wajdibejaoui50@gmail.com>
2024-07-25 18:43:32 +0100

    Customize the theme to get darker grays

---
+++ src/Data/genres.ts
@@ -0,0 +1,98 @@
+export default [
+    {
+        "_id": "1",
+        "name": "Action",
+        "slug": "action",
+        "image_background": "https://wallpaperaccess.com/full/8.jpg"
+    },
+    {
+        "_id": "2",
+        "name": "Indie",
+        "slug": "indie",
+        "image_background": "https://wallpaperaccess.com/full/12.jpg"
+    },
+    {
+        "_id": "3",
+        "name": "Adventure",
+        "slug": "adventure",
+        "image_background": "https://wallpaperaccess.com/full/1.jpg"
+    },
+    {
+        "_id": "4",
+        "name": "RPG",
+        "slug": "rpg",
+        "image_background": "https://www.pexels.com/photo/18.jpg"
+    },
+    {
+        "_id": "5",
+        "name": "Strategy",
+        "slug": "strategy",
+        "image_background": "https://pixabay.com/photo/26.jpg"
+    },
+    {
+        "_id": "6",
+        "name": "Shooter",
+        "slug": "shooter",
+        "image_background": "https://wallpaperaccess.com/full/23.jpg"
+    },
+    {
+        "_id": "7",
+        "name": "Casual",
+        "slug": "casual",
+        "image_background": "https://pixune.com/photo/41.jpg"
+    },
+    {
+        "_id": "8",
+        "name": "Simulation",
+        "slug": "simulation",
+        "image_background": "https://en.wikipedia.org/photo/38.jpg"
+    },
+    {
+        "_id": "9",
+        "name": "Puzzle",
+        "slug": "puzzle",
+        "image_background": "https://www.freepik.com/photo/35.jpg"
+    },
+    {
+        "_id": "10",
+        "name": "Platformer",
+        "slug": "platformer",
+        "image_background": "https://itch.io/photo/31.jpg"
+    },
+    {
+        "_id": "11",
+        "name": "Arcade",
+        "slug": "arcade",
+        "image_background": "https://example.com/arcade.jpg"
+    },
+    {
+        "_id": "12",
+        "name": "Sports",
+        "slug": "sports",
+        "image_background": "https://example.com/sports.jpg"
+    },
+    {
+        "_id": "13",
+        "name": "Racing",
+        "slug": "racing",
+        "image_background": "https://example.com/racing.jpg"
+    },
+    {
+        "_id": "14",
+        "name": "Multiplayer",
+        "slug": "multiplayer",
+        "image_background": "https://example.com/multiplayer.jpg"
+    },
+    {
+        "_id": "15",
+        "name": "Fighting",
+        "slug": "fighting",
+        "image_background": "https://example.com/fighting.jpg"
+    },
+    {
+        "_id": "16",
+        "name": "Board Games",
+        "slug": "board-games",
+        "image_background": "https://example.com/board-games.jpg"
+    }
+]
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,14 +1,14 @@
 
-import useData from "./useData";
-
+import genres from "../Data/genres"
 export interface Genre {
   _id: string;
   name: string;
   slug: string;
-  game_count: number;
+  // game_count: number;
   image_background: string;
 }
 
-const useGenres = () => useData<Genre>('/genres')
+// const useGenres = () => useData<Genre>('/genres')
+const useGenres = () => ({data : genres, isLoading : false, error : null})
 
 export default useGenres;
